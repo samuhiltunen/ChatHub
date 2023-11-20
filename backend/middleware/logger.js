074b903c2e48d8938e7b61730c5c8ada224d4779
markus <mag3768@gmail.com>
2023-11-20 21:30:55 +0200

    fixed hanging

--- backend/middleware/logger.js
+++ backend/middleware/logger.js
@@ -1,6 +1,5 @@
 // Logger middleware
 const logger = (req, res, next) => {
-    next();
     console.log(`
         [${req.currtime()}] ${req.method} ${req.url} requested from ${req.ip}. 
         \n Response sent with status ${res.statusCode}
