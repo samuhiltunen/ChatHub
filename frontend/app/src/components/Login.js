08aa5c5c1b4d95017c53f6eee0b348ddad3e89d9
samuh <samu.hiltunen@metropolia.fi>
2023-11-21 01:24:23 +0200

    storen accessToken

--- frontend/app/src/components/Login.js
+++ frontend/app/src/components/Login.js
@@ -12,17 +12,16 @@ export default function Login() {
 
     const options = {
       method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({name: username, password: password})
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name: username, password: password })
     };
 
     try {
       const response = await fetch('https://auth.chathub.kontra.tel/login', options);
       if (response.ok) {
-        
         const data = await response.json();
         console.log("OK");
-        localStorage.setItem('token', data.token);
+        localStorage.setItem('token', data.accessToken);
         navigate('/main');
       } else {
         console.error("Server responded with status:", response.status);
