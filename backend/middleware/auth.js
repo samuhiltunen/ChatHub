0835c1542fab01e6928020494421869fc50b6a3e
markus <mag3768@gmail.com>
2023-11-27 04:55:28 +0200

    Fix token verification logic in auth middleware

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,9 +4,7 @@ const jwt = require('jsonwebtoken');
 function auth(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
-    // Check if token exists
-    if(token == null) res.sendStatus(401).json({error: "Unauthorized"});
-    
+
     // Dev ip
     if(req.ip == '::1') {
         console.log('dev route');
@@ -17,14 +15,19 @@ function auth(req, res, next) {
         next();
     }
     
-    // Verify token
-    jwt.verify(token, process.env.ACCESS_TOKEN, (err, payload) => {
-        if(err) res.sendStatus(403).json({error: "Forbidden"});
-        else {
-            req.user = payload.user;
-            next();
-        }
-    });
+    // Check if token exists
+    if(token == null) 
+        res.sendStatus(401).json({error: "Unauthorized"});
+    else {
+        // Verify token
+        jwt.verify(token, process.env.ACCESS_TOKEN, (err, payload) => {
+            if(err) res.sendStatus(403).json({error: "Forbidden"});
+            else {
+                req.user = payload.user;
+                next();
+            }
+        });
+    }
     return;
 }
 
