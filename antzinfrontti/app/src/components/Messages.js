90bfcceba8e2912b70421cf3ef886c0f0d9b12f9
antonlai <antonlai@metropolia.fi>
2023-12-04 22:11:40 +0200

    threads search toimii

--- antzinfrontti/app/src/components/Messages.js
+++ antzinfrontti/app/src/components/Messages.js
@@ -5,15 +5,18 @@ import Message from './Message';
 const messages = [{
     id: 1,
     text: "Test Message",
-    time: "20.11 klo 12.25"
+    time: "20.11 klo 12.25",
+    sender:"userA"
 }, {
     id : 2,
     text: "Test Message2",
-    time: "20.11 klo 12.25"
+    time: "20.11 klo 12.25",
+    sender: "userB"
 }, {
     id: 3,
     text: "Test Message3",
-    time: "20.11 klo 12.25"
+    time: "20.11 klo 12.25",
+    sender: "UserA"
 }
 ];
 
--- antzinfrontti/app/src/components/Threads.js
+++ antzinfrontti/app/src/components/Threads.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Thread from './Thread';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserPlus, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 // TODO: Data from backend
-const threads = [{
+const threadsData = [{
     id: 1,
     user: "TestUser",
     time: "20.11 klo 12.25"
@@ -20,6 +20,23 @@ const threads = [{
 ];
 
 export default function Threads() {
+
+    const [searchText, setSearchText] = useState('');
+    const [filteredThreads, setFilteredThreads] = useState(threadsData);
+
+    const handleSearch = () => {
+        const filtered = threadsData.filter(thread =>
+            thread.user.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setFilteredThreads(filtered);
+    };
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
     return (
       <div id={"vittu"}>
           <div className="user-searchbox">
@@ -27,14 +44,17 @@ export default function Threads() {
                   type="text"
                   id="username-search"
                   placeholder="search by username"
+                  value={searchText}
+                  onChange={(e) => setSearchText(e.target.value)}
+                  onKeyPress={handleKeyPress}
               />
               <div id={"userSearchButtons"}>
-              <button> <FontAwesomeIcon icon={faMagnifyingGlass}/> </button>
+              <button onClick={handleSearch}> <FontAwesomeIcon icon={faMagnifyingGlass}/> </button>
               <button> <FontAwesomeIcon icon={faUserPlus}/> </button>
               </div>
           </div>
           <h1>My Threads</h1>
-          {threads.map(thread => {
+          {filteredThreads.map(thread => {
               return <Thread user={thread.user} time={thread.time} key={thread.id} />;
           })}
       </div>
