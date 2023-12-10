8abcf3cbb164e0d23be0ba6ea7e3e527092a09f1
samuh <samu.hiltunen@metropolia.fi>
2023-12-11 01:33:50 +0200

    create thread

--- frontend/app/src/components/Main.js
+++ frontend/app/src/components/Main.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import "../css/main.css";
 import { Link } from 'react-router-dom';
 import Threads from './Threads';
@@ -27,44 +27,42 @@ export default function Main() {
                 setMainVisible(true);
             } else {
                 setAsideVisible(false);
-
             }
         };
-
         handleResize();
         window.addEventListener('resize', handleResize);
-
         return () => {
             window.removeEventListener('resize', handleResize);
         };
     }, []);
-
 
     return (
         <>
             <Header toggleAside={toggleAside} />
             <section>
                 <aside className={asideVisible ? 'show-aside' : 'hide-aside'}>
-                    <button onClick={() => setShowCreateThread(true)}>Create new thread</button>
-                    {showCreateThread && <CreateThread setShowCreateThread={setShowCreateThread}/>}
+                    <button onClick={() => setShowCreateThread(!showCreateThread)}>
+                        {showCreateThread ? "Close" : "Create new thread"}
+                    </button>
+                    {showCreateThread && <CreateThread setShowCreateThread={setShowCreateThread} isOpen={showCreateThread} />}
                     <Threads />
-                    <ProfileInAside/>
+                    <ProfileInAside />
                 </aside>
                 <main className={mainVisible ? null : 'hide-main'}>
                     <div id="chat" className="chat-container">
                         {/* Chat messages go here */}
-                        <Messages/>
+                        <Messages />
                     </div>
                     {/* Input area for typing messages */}
                     <div className="messagebox">
                         <div className={"messagewrap"}>
                             <button><FontAwesomeIcon icon={faPaperclip} size={"lg"} /></button>
-                        <textarea  id="messageInput" placeholder="Type your message" />
-                        <button ><FontAwesomeIcon icon={faPaperPlane} size={"lg"} /></button>
-                            </div>
+                            <textarea id="messageInput" placeholder="Type your message" />
+                            <button ><FontAwesomeIcon icon={faPaperPlane} size={"lg"} /></button>
+                        </div>
                     </div>
                 </main>
             </section>
         </>
-);
+    );
 }
