a52f558ce6805f9151d7240376716a3f13406cde
markus <mag3768@gmail.com>
2023-12-12 14:21:38 +0200

    Added logging to loader.js

--- backend/loader.js
+++ backend/loader.js
@@ -1,5 +1,5 @@
 // This script loads all modules automatically to avoid having to import them manually in every file.
-// Loader ignores files that have '// Loader ignore' in them.
+// Loader ignores files that have '// Loader ignore' or no module export in them.
 
 const path = require('path');
 const fs = require('fs');
@@ -23,7 +23,7 @@ for(const dir of loadDir) {
 
         // Check if file is to be ignored or doesn't have module.exports
         if(fileBuffer.toString().includes('// Loader ignore') || !fileBuffer.toString().includes('module.exports')) {
-            console.log(`Ignoring ${file}`);
+            console.log(`LOADER: Ignoring ${file}`);
             return;
         }
 
@@ -43,6 +43,12 @@ for(const dir of loadDir) {
                 break;
         }
     });
+
+    // Log loaded modules ignore if dir is empty
+    if(Object.keys(database).length > 0) console.log(`LOADER: Loaded ${Object.keys(database).length} modules to database`);
+    if(Object.keys(middleware).length > 0) console.log(`LOADER: Loaded ${Object.keys(middleware).length} modules to middleware`);
+    if(Object.keys(routes).length > 0) console.log(`LOADER: Loaded ${Object.keys(routes).length} modules to routes`);
+
     switch(dir) {
         case 'database':
             module.exports.database = database;
